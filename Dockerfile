175d4af36f069f94e7b92aca41f07619de88b414
Matthew LaRocca <mlarocca.cpp@gmail.com>
2024-01-16 17:13:30 -0800

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,46 @@
-from ubuntu:focal as cpp_builder
+# Build with: docker build -t matthewcpp/framework64-audio .
+# On Apple silicon: docker build -t matthewcpp/framework64-audio --platform linux/amd64 .
 
-COPY tools /tools
+from ubuntu:jammy as cpp_builder
 
-RUN apt-get update && \
-	DEBIAN_FRONTEND="noninteractive" apt-get install -y build-essential cmake git libsndfile-dev && \
-	cd /tools && mkdir build && cd build && \
-	cmake .. && \
-	cmake --build . --target json2ins --config Release && \
-	cmake --build . --target snd2aiff --config Release
+ENV VCPKG_GITHUB_REPO="https://github.com/microsoft/vcpkg.git"
+ENV VCPKG_DIR=/lib/vcpkg
 
-
-from ubuntu:focal
+COPY src /src
 
 RUN apt-get update && \
-	dpkg --add-architecture i386 && \
-	DEBIAN_FRONTEND="noninteractive" apt-get install -y wget gnupg software-properties-common unzip p7zip cabextract curl nano libsndfile1 wine && \
-	curl -fsSL https://deb.nodesource.com/setup_16.x | bash - && \
-	apt-get install -y nodejs && \
-	apt-get install -y wine32 && \
-	wineconsole --help && \
-	mkdir /src /dest /job /fw64 && \
-	rm -rf /var/lib/apt/lists/*
+    DEBIAN_FRONTEND="noninteractive" apt-get install -y build-essential cmake git libsndfile-dev curl zip && \
 
-COPY --from=cpp_builder /tools/build/json2ins/json2ins /tools/build/snd2aiff/snd2aiff /fw64/
+    mkdir /lib && cd /lib && git clone $VCPKG_GITHUB_REPO && \
+    cd $VCPKG_DIR && ./bootstrap
+
+    cd /tools && mkdir build && cd build && ./bootstrap-vcpkg.sh \
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=$VCPKG_DIR/scripts/buildsystems/vcpkg.cmake&& \
+    cmake --build . --target json2ins --config Release && \
+    cmake --build . --target snd2aiff --config Release
+
+
+from ubuntu:jammy
+
+ARG NODE_MAJOR_VERSION=20
+
+RUN apt-get update && \
+    dpkg --add-architecture i386 && \
+    DEBIAN_FRONTEND="noninteractive" apt-get install -y wget gnupg software-properties-common unzip p7zip cabextract curl nano wine && \
+    apt-get install -y wine32 && \
+    wineconsole --help && \
+    mkdir /src /dest /job /fw64 && \
+
+    apt-get install -y ca-certificates curl gnupg && \
+    mkdir -p /etc/apt/keyrings && \
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg && \
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR_VERSION}.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list && \
+    apt-get update && apt-get install -y nodejs && \
+    
+    rm -rf /var/lib/apt/lists/*
+
+
+COPY --from=cpp_builder /src/build/json2ins/json2ins /src/build/snd2aiff/snd2aiff /fw64/
 COPY . /
 
 ENTRYPOINT ["/fw64/entrypoint.sh"]
--- src/snd2aiff/main.cpp
+++ src/snd2aiff/main.cpp
@@ -1,19 +1,19 @@
 #include <sndfile.h>
 
 #include <array>
-#include <experimental/filesystem>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <string>
 
-namespace fs = std::experimental::filesystem;
+#include "nlohmann/json.hpp"
 
 #define WORKING_BUFFER_LEN 1024
 std::array<double, WORKING_BUFFER_LEN> working_buffer;
 
-bool check_paths(const fs::path& src_path, const fs::path& dest_dir);
-fs::path get_dest_path(const fs::path& src_path);
-void convert_to_aiff(SNDFILE* source_file, SNDFILE* dest_file);
-void convert_stereo_to_mono_aiff(SNDFILE* source_file, SNDFILE* dest_file);
+bool check_paths(const std::filesystem::path& src_path, const std::filesystem::path& dest_dir);
+bool process_file(const std::filesystem::path& src_path, const std::filesystem::path& dest_audio_path);
+bool write_info_file(const std::filesystem::path& src_path, const std::filesystem::path& dest_path);
 
 int main(int argc, char** argv) {
     if (argc < 3) {
@@ -21,87 +21,46 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    fs::path src_path = argv[1];
-    fs::path dest_dir = argv[2];
+    std::filesystem::path src_path = argv[1];
+    std::filesystem::path dest_dir = argv[2];
 
-    if (!check_paths(src_path, dest_dir))
-        return 1;
-
-    const auto src_ext = src_path.extension().string();
-
-    SF_INFO source_info = {0};
-    SNDFILE* source_file = sf_open(src_path.c_str(), SFM_READ, &source_info);
-
-    if (!source_file) {
-        std::cout << "Error opening file: " << src_path << std::endl; 
-        std::cout << sf_strerror(NULL) << std::endl;
+    if (!check_paths(src_path, dest_dir)) {
         return 1;
     }
 
-    if (source_info.channels > 2) {
-        std::cout << "Input file contains " << source_info.channels << " channels.  Max supported channels is 2." << std::endl;
+    auto dest_audio_path = (dest_dir / src_path.filename()).replace_extension(".aiff");
+
+    if (!process_file(src_path, dest_audio_path)) {
         return 1;
     }
 
-    fs::path dest_path = dest_dir / src_path.stem().string().append(".aiff");
+    auto dest_info_path = dest_audio_path;
+    dest_info_path.replace_extension(".json");
 
-    if (src_ext == ".aiff" && source_info.channels == 1) {
-        std::cout << "Copy " << src_path << " to " << dest_path << " (no processing necessary)" << std::endl;
-        sf_close(source_file);
-        fs::copy_file(src_path, dest_path);
-        return 0;
-    }
-
-    SF_INFO dest_info = source_info;
-    dest_info.channels = 1;
-    dest_info.format = SF_FORMAT_AIFF | SF_FORMAT_PCM_16;
-    SNDFILE* dest_file = sf_open(dest_path.c_str(), SFM_WRITE, &dest_info);
-
-    if (!dest_file) {
-        std::cout << "Unable to open output: " << dest_path << std::endl;
-        std::cout << sf_strerror(NULL);
+    if (!write_info_file(dest_audio_path, dest_info_path)) {
         return 1;
     }
-
-    std::cout << "Converting " << src_path << " to aiff" << std::endl;
-
-    if (source_info.channels == 1) {
-        convert_to_aiff(source_file, dest_file);
-    }
-    else {
-        std::cout << "Resampling stereo to mono" << std::endl;
-        convert_stereo_to_mono_aiff(source_file, dest_file);
-    }
-
-    sf_close(source_file);
-    sf_close(dest_file);
 
     return 0;
 }
 
-bool check_paths(const fs::path& src_path, const fs::path& dest_dir) {
-    if (!fs::exists(src_path)) {
+bool check_paths(const std::filesystem::path& src_path, const std::filesystem::path& dest_dir) {
+    if (!std::filesystem::exists(src_path)) {
         std::cout << "Input " << src_path << " does not exist" << std::endl;
         return false;
     }
 
-    if (!fs::is_regular_file(src_path)) {
+    if (!std::filesystem::is_regular_file(src_path)) {
         std::cout << "Input " << src_path << " is not a regular file." << std::endl;
         return false;
     }
 
-    if (!fs::is_directory(dest_dir)) {
+    if (!std::filesystem::is_directory(dest_dir)) {
         std::cout << "Destination " << dest_dir << " is not a directory" << std::endl;
         return false;
     }
 
     return true;
-}
-
-fs::path get_dest_path(const fs::path& src_path) {
-    std::string filename = src_path.stem();
-    filename.append(".aiff");
-    return src_path.parent_path() / filename;
 }
 
 void convert_to_aiff(SNDFILE* source_file, SNDFILE* dest_file) {
@@ -125,4 +84,83 @@ void convert_stereo_to_mono_aiff(SNDFILE* source_file, SNDFILE* dest_file) {
 
         sf_write_double (dest_file, working_buffer.data(), read_count / 2) ;
     }
+}
+
+bool process_file(const std::filesystem::path& src_path, const std::filesystem::path& dest_audio_path) {
+    SF_INFO source_info = {0};
+    SNDFILE* source_file = sf_open(src_path.c_str(), SFM_READ, &source_info);
+
+    if (!source_file) {
+        std::cout << "Error opening file: " << src_path << std::endl; 
+        std::cout << sf_strerror(NULL) << std::endl;
+        return false;
+    }
+
+    if (source_info.channels > 2) {
+        std::cout << "Input file contains " << source_info.channels << " channels.  Max supported channels is 2." << std::endl;
+        return false;
+    }
+
+    if (src_path.extension() == ".aiff" && source_info.channels == 1) {
+        std::cout << "Copy " << src_path << " to " << dest_audio_path << " (no processing necessary)" << std::endl;
+        sf_close(source_file);
+        std::filesystem::copy_file(src_path, dest_audio_path);
+        return true;
+    }
+
+    SF_INFO dest_info = {0};
+    dest_info.channels = 1;
+    dest_info.samplerate = source_info.samplerate;
+    dest_info.format = SF_FORMAT_AIFF | SF_FORMAT_PCM_16;
+    SNDFILE* dest_file = sf_open(dest_audio_path.c_str(), SFM_WRITE, &dest_info);
+
+    if (!dest_file) {
+        std::cout << "Unable to open output: " << dest_audio_path << std::endl;
+        std::cout << sf_strerror(NULL);
+        return 1;
+    }
+
+    std::cout << "Converting " << src_path << " to aiff" << std::endl;
+
+    if (source_info.channels == 1) {
+        convert_to_aiff(source_file, dest_file);
+    }
+    else {
+        std::cout << "Converting stereo to mono" << std::endl;
+        convert_stereo_to_mono_aiff(source_file, dest_file);
+    }
+
+    sf_close(source_file);
+    sf_close(dest_file);
+
+    return true;
+}
+
+bool write_info_file(const std::filesystem::path& src_path, const std::filesystem::path& dest_path) {
+    SF_INFO source_info = {0};
+    SNDFILE* source_file = sf_open(src_path.c_str(), SFM_READ, &source_info);
+
+    if (!source_file) {
+        std::cout << "Failed to open converted sound file for writing: " << src_path << std::endl;
+        return false;
+    }
+
+    sf_close(source_file);
+
+    std::ofstream json_file(dest_path);
+
+    if (!json_file) {
+        std::cout << "Failed to open info file for writing: " << dest_path << std::endl;
+        return false;
+    }
+
+    double seconds = static_cast<double>(source_info.frames) / static_cast<double>(source_info.samplerate); 
+
+    nlohmann::json info_json = {
+        {"duration", seconds}
+    };
+
+    json_file << info_json.dump(4) << std::endl;
+
+    return true;
 }
